bb8af6f5120bbd7f96d39ff741a269a4bd758d5d
noone787 <kubacieslik5407@gmail.com>
2025-02-05 15:22:02 +0100

    nigg
    er

--- statki.py
+++ statki.py
@@ -1,1 +1,33 @@
-# siema mordo
+
+running = True
+
+plansza = ["-","-","-","-","-","-","-","-","-",
+           "-","-","-","-","-","-","-","-","-",
+           "-","-","-","-","-","-","-","-","-",
+           "-","-","-","-","-","-","-","-","-",
+           "-","-","-","-","-","-","-","-","-",
+           "-","-","-","-","-","-","-","-","-",
+           "-","-","-","-","-","-","-","-","-",
+           "-","-","-","-","-","-","-","-","-",
+           "-","-","-","-","-","-","-","-","-",]
+
+def PokazPlanszy(plansza):
+    for i in range(1,82):
+        print(plansza[i - 1 ],end="|")
+        if i%9 == 0:
+            print()
+
+Lista = ["O O","O O","O O O","O O O","O O O O","O O O O O"]
+liczba_statkow = 6
+
+def Ustawianie(liczba_statkow):
+    for i in range(0,liczba_statkow + 1):
+        print(Lista[i])
+    rząd = int(input("podaj rząd (od 1 do 9)"))
+    kolumna = int(input("podaj kolumnę (od 1 do 9)"))
+    plansza[rząd * 9 + kolumna - 1] = "O"
+    
+
+Ustawianie(liczba_statkow)
+while running:
+    PokazPlanszy(plansza)
